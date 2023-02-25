c8bce5ff6017351f440ceaff33c191666cc5426a
george <george35@utp.edu.co>
2023-02-25 12:15:42 -0500

    Create, Get, Delete y Update de product

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,35 @@
-export class CreateProductDto {}
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional,
+    IsPositive, MinLength, IsString 
+} from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional() 
+    stock?: number;
+
+    @IsString({each: true})  // Cada uno de los elementos tiene que cumplir la condicion de ser un string
+    @IsArray()
+    sizes: string[];
+
+    @IsIn(['men', 'women', 'kid', 'unisex']) // Obliga a que en el arreglo venga con esos valores 
+    gender: string;
+}
