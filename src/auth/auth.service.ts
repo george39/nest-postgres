0f182b94ae870d6801666a651d48fb3121178d67
george <george35@utp.edu.co>
2023-02-27 16:35:27 -0500

    crear usuario 'incompleto'

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ){}
+  
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new auth';
+    try {
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   
